358da410593995fc639ae134571a8754cdf93ab2
Alex Reinking <alex.reinking@gmail.com>
2021-04-14 12:31:41 -0700

    Use POSIX threading for MinGW

--- cmake/toolchain.mingw-w64.cmake
+++ cmake/toolchain.mingw-w64.cmake
@@ -5,8 +5,8 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR AMD64)
 
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
 
 set(CMAKE_C_FLAGS_INIT "-static-libgcc -static-libstdc++")
 set(CMAKE_CXX_FLAGS_INIT "-static-libgcc -static-libstdc++")
